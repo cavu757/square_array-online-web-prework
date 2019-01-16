4dc8a4abf1d9cceb5297d7521f8b46c2c7c743c1
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-16 04:00:52 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,10 @@
-require 'pry'
+
 
 def square_array(numbers)  
   square_array=[]
   numbers.each do |number|
     square = number**2 
     square_array.push(square)
-    binding.pry
   end
   square_array
 end
